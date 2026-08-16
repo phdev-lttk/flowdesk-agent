4df8898a51f3b7b8a6a064661e0cab7fd09a6629
phdev-lttk <pablohenriquesouzaa@gmail.com>
2026-08-16 17:41:47 -0300

    fix: CORS to production

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="AI Document Agent")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
