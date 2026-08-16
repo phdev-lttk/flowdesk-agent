3a775e4e29cd379806444451b9a2d8aedfcb3eb6
phdev-lttk <pablohenriquesouzaa@gmail.com>
2026-08-16 17:36:48 -0300

    fix: App.jsx

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     setQuestion("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/ask", {
+      const response = await fetch("https://flowdesk-agent.onrender.com/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
